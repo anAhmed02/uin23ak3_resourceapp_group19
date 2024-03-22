d1f32ce11523ea4e41d9f1c3c6a2998a7c7815cc
anAhmed02 <Ahmednurahmed02@gmail.com>
2024-03-22 02:34:27 +0100

    noen endringer
    la inn html, ressurser og css

--- React projekt/my-app/src/min-react-app/src/App.js
+++ React projekt/my-app/src/min-react-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; //importerer filer fra app.css filen
+import React, { useState } from 'react'; //importerer react og usestate fra react-pakken
+import { resources } from './components/ressurser'; //importerer ressursene fra ressurser.js-filen
 
-function App() {
+// Definerer App-komponenten som en funksjonell komponent
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState('HTML'); //Oppretter en tilstand for den valgte kategorien ('selectedCategory') med 'HTML' som startverdi
+
+  const resource = resources.find(r => r.category.toLowerCase() === selectedCategory.toLowerCase()); //henter ressurser som matcher den valgte kategorien fra ressurser
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <nav>
+        {resources.map((res, index) => (
+          <button key={index} onClick={() => setSelectedCategory(res.category)}>
+            {res.category}
+          </button>
+        ))}
+      </nav>
+
+      {resource && (
+        <article>
+          <h1>{resource.category}</h1>
+          <p>{resource.text}</p>
+          <ul>
+            {resource.sources.map(({ title, url }, index) => (
+              <li key={index}>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                  {title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </article>
+      )}
+
+      {!resource && <p>Ingen ressurser funnet for kategorien {selectedCategory}.</p>}
     </div>
   );
-}
+};
 
-export default App;
+export default App; //eksporterer app-komponentet som standard
